282fc3d75c671adbd6c4b3ea82118b7a336b2de6
Zach Brown <bigcountry66789@gmail.com>
2026-03-01 12:10:02 -0600

    Saving at mid point

---
+++ Module2/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Module2;
+
+var zach = new Student();
+zach.Name = "Zach";
+
+var john = new Student();
+john.Name = "John";
+
+var frank = new Student();
+frank.Name = "Frank";
+
+var tom = new Student();
+tom.Name = "Tom";
+
+var joey = new Student();
+joey.Name = "Joey";
+
+var students = new List<Student>();
+
+
---
+++ Module2/Student.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+
+namespace Module2;
+
+public class Student
+{
+    public string Name { get; set; } //Property called Name
+    public int ID { get; set; } //Property called ID
+    public List<double> Grades { get; set; } = new(); //List Property called Grades
+
+    public void AddGrade(double grade) //The first method for grade
+    {
+        Grades.Add(grade);
+    }
+
+public void AddGrade(params double[] grades) //This method is for grades 
+    {
+        Grades.AddRange(grades);
+    }
+
+    public double CalculateAverageGrade()//The method with no parameters that will return the avg or 0
+    {
+        if (Grades.Count == 0)
+        {
+            return 0;
+        }
+
+        return Grades.Average();
+    }
+}
